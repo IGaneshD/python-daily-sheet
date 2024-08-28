b607f8cc9f56a95701bb8a7a098b824453b505e4
Ganesh Deshmukh <rajdhani.blog@gmai.com>
2024-08-28 19:45:19 +0530

    python json

--- match_function_check.py
+++
@@ -1,9 +0,0 @@
-import re
-
-strings = ["b", "ab", "bba"]
-pattern = "b$"
-
-# compiled = re.compile(pattern)
-
-for string in strings:
-    print(f"{string}\t", "Matched" if re.match(pattern,string) else "No Match")
